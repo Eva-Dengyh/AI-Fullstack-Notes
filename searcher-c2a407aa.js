d41cc2608ec76970d9428ca2abb8bb7ffedb8d36
Eva-Dengyh <Eva-Dengyh@users.noreply.github.com>
2026-04-13 13:27:25 +0000

    deploy: e93764b98b2a615656331acb6570f60c135bd663

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bd8af928.js',
+                path_to_root + 'searchindex-68d2c33e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
